d7bb0aecfc92460d946f1583e4ca3fe7705956d3
Tejas Ladhani <67834407+Tejas-Ladhani@users.noreply.github.com>
2022-01-08 20:29:41 +0530

    txt to speech basic

--- src/main.py
+++ src/main.py
@@ -1,2 +1,16 @@
 import os
+# version 2.71 
+import pyttsx3
+import speech_recognition as sr
 
+engine = pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+# print(voices)
+engine.setProperty('voices',voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+if __name__=="__main__":
+    speak("Hello there")
